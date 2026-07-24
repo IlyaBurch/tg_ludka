26ebb348dcb9a302435e8d78fe27050e85de174d
Ilya Burch <116872781+IlyaBurch@users.noreply.github.com>
2026-07-24 17:15:02 +0300

    Update dice.js

--- src/handlers/dice.js
+++ src/handlers/dice.js
@@ -66,7 +66,7 @@ module.exports = function (bot) {
     }
     stmts.updateSpins.run(player.free_spins, player.points, userId, chatId);
 
-    await sleep(1500);
+    await sleep(1600);
 
     const symbols = decodeSlots(ctx.message.dice.value);
     const { payout, jackpot } = calcWin(symbols);
